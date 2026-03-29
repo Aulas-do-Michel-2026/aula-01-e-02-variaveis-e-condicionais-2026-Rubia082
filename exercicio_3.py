49a594963587398ba1ad3adece9e14a7adab66f8
Rubia082 <rubia082@gmail.com>
2026-03-29 10:32:45 -0300

    Update exercicio_3.py

--- exercicio_3.py
+++ exercicio_3.py
@@ -1,4 +1,3 @@
-"""
 #### Exercício 3
 
 Receba um número inteiro de um usuário. Se ele for par, imprima "Par". Se não, imprima "Ímpar".
@@ -17,3 +16,10 @@ Digite um número:
 
 Dica: Lembre do comando de resto da divisão inteira!
 """
+
+numero= int(input("Digite um número: "))
+
+if numero % 2 == 0:
+    print("Par")
+else: 
+    print("Ímpar")
